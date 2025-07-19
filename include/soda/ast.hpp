9af92b7f1516508525970a489ef586620dbb459c
Paul Zupan <paul.zupan.13@gmail.com>
2025-07-19 11:52:15 -0700

    Settle on AST representation

--- include/soda/ast.hpp
+++ include/soda/ast.hpp
@@ -1,113 +1,62 @@
 #ifndef SODA_AST_HPP
 #define SODA_AST_HPP
 
-#include <memory>
+#include <cstdint>
 #include <optional>
 #include <string>
+#include <variant>
 #include <vector>
 
 namespace soda {
 
-class Statement;
-class Expression;
-class Type;
+/////////////////
+// Expressions //
+/////////////////
 
-class ASTNode {
-public:
-  virtual ~ASTNode() = default;
+struct Identifier {
+  std::string name;
 };
 
-class Statement : public ASTNode {
-public:
-  virtual ~Statement() = default;
+struct IntegerLiteral {
+  std::int32_t value;
 };
 
-class Expression : public ASTNode {
-public:
-  virtual ~Expression() = default;
+using Expression = std::variant<Identifier, IntegerLiteral>;
+
+////////////////
+// Statements //
+////////////////
+
+struct BlockStatement;
+struct ReturnStatement;
+
+using Statement = std::variant<BlockStatement, ReturnStatement>;
+
+struct BlockStatement {
+  std::vector<Statement> statements;
 };
 
-class Declaration : public ASTNode {
-public:
-  virtual ~Declaration() = default;
+struct ReturnStatement {
+  Expression return_value;
 };
 
-class Type : public ASTNode {
-public:
-  virtual ~Type() = default;
+//////////////////
+// Declarations //
+//////////////////
+
+struct TypedIdentifier {
+  std::string name;
+  std::string type;
 };
 
-class Identifier : public Expression {
-public:
-  explicit Identifier(std::string name) : m_name{std::move(name)} {}
-  const char *get_name() const { return m_name.data(); }
-
-private:
-  std::string m_name;
+struct FunctionDeclaration {
+  std::string name;
+  std::optional<std::string> return_type;
+  std::vector<TypedIdentifier> params;
+  BlockStatement body;
 };
 
-class IntegerLiteral : public Expression {
-public:
-  explicit IntegerLiteral(std::int32_t value) : m_value{value} {}
-  std::int32_t get_value() const { return m_value; }
-
-private:
-  std::int32_t m_value;
-};
-
-// Parameter declaration
-class Parameter {
-public:
-  Parameter(std::string name, std::unique_ptr<Type> type)
-      : m_name{std::move(name)}, m_type{std::move(type)} {}
-
-  const char *get_name() const { return m_name.data(); }
-  const Type *get_type() const { return m_type.get(); }
-
-private:
-  std::string m_name;
-  std::shared_ptr<Type> m_type;
-};
-
-// Statements
-class BlockStatement : public Statement {
-public:
-  explicit BlockStatement(std::vector<std::unique_ptr<Statement>> statements)
-      : m_statements{std::move(statements)} {}
-  const std::vector<std::unique_ptr<Statement>> &get_statements() const { return m_statements; }
-
-private:
-  std::vector<std::unique_ptr<Statement>> m_statements;
-};
-
-class ReturnStatement : public Statement {
-public:
-  explicit ReturnStatement(std::unique_ptr<Expression> expression)
-      : m_return_value{std::move(expression)} {}
-  const Expression *get_return_value() const { return m_return_value.get(); }
-
-private:
-  std::unique_ptr<Expression> m_return_value;
-};
-
-class FunctionDeclaration : public Declaration {
-public:
-  FunctionDeclaration(std::string name, std::optional<Type> returnType,
-                      std::vector<Parameter> parameters, std::unique_ptr<BlockStatement> body)
-      : m_name(std::move(name)), m_return_type(std::move(returnType)),
-        m_parameters(std::move(parameters)), m_body(std::move(body)) {}
-
-  const std::string &get_name() const { return m_name; }
-  const std::optional<Type> &get_return_type() const { return m_return_type; }
-  const std::vector<Parameter> &get_parameters() const { return m_parameters; }
-  // const BlockStatement &get_body() const { return m_body; }
-
-private:
-  std::string m_name;
-  std::optional<Type> m_return_type;
-  std::vector<Parameter> m_parameters;
-  std::unique_ptr<BlockStatement> m_body;
-};
+using Declaration = std::variant<FunctionDeclaration>;
 
 } // namespace soda
 
